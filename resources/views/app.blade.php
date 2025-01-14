5fba08fbf4cdbcc3dbf024389f38ff65f73e1d11
anouarmriss <anouarmriss@gmail.com>
2025-01-15 00:29:29 +0100

    Force HTTPS for assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,19 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E-Ticket System</title>
-    @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('build/assets/app-D9ezDlAS.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-D9ezDlAS.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    <script src="{{ asset('build/assets/app-aLDTWd2_.js') }}"></script>
-    <script>
-        console.log('Page loaded');
-        window.addEventListener('load', function() {
-            console.log('Window loaded');
-            const appElement = document.querySelector('#app');
-            console.log('App element:', appElement);
-        });
-    </script>
+    <script src="{{ secure_asset('build/assets/app-aLDTWd2_.js') }}"></script>
+    @vite(['resources/js/app.js'])
 </body>
 </html>
