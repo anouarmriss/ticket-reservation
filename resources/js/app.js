619c263c5e0672678ef32043dfd45afda3e92e89
anouarmriss <anouarmriss@gmail.com>
2025-01-15 09:14:57 +0100

    Update frontend configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "./views/HomeView.vue";
 import ReservationForm from "./components/ReservationForm.vue";
-import ConfirmationView from "./views/ConfirmationView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
             path: "/reservation",
             name: "reservation",
             component: ReservationForm,
-        },
-        {
-            path: "/confirmation",
-            name: "confirmation",
-            component: ConfirmationView,
         },
     ],
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,19 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>E-Ticket System</title>
-    @viteReactRefresh
+    <title>Tourist Site</title>
     @vite(['resources/js/app.js'])
 </head>
 <body>
     <div id="app"></div>
-    <script>
-        console.log('DOM loaded');
-        document.addEventListener('DOMContentLoaded', () => {
-            console.log('Document ready');
-            const app = document.getElementById('app');
-            console.log('App element:', app);
-        });
-    </script>
 </body>
 </html>
